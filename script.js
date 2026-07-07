47c297fac3e0d4614cedbab3edfc0bd65264662e
muhammadakbarfadilah-dot <muhammadakbarfadilah@smkwikrama.sch.id>
2026-07-07 15:52:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Website Portofolio Muhammad Akbar Fadilah berhasil dimuat!");
+window.addEventListener("scroll",function(){
+
+const nav=document.querySelector("nav");
+
+if(window.scrollY>50){
+
+nav.style.background="rgba(15,23,42,.95)";
+
+}else{
+
+nav.style.background="rgba(255,255,255,.05)";
+
+}
+
+});
